89843a87014e7a46331d9ecfbe64ff035da0eb77
Hasan Ali Alsamman <hassan.alsamman2015@gmail.com>
2022-09-27 15:15:55 +0300

    4

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+/* eslint-disable consistent-return */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require("express");
-const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "build")));
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
+app.listen(port, (err) => {
+  if (err) return console.log(err);
+  console.log(`Server running on port ${port}`);
 });
-
-app.listen(PORT);
