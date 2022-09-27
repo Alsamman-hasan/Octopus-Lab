3e65ad4ac543e72c3ad247e35f251d043555118e
Hasan Ali Alsamman <hassan.alsamman2015@gmail.com>
2022-09-27 16:46:06 +0300

    test

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(port, (err) => {
+  // eslint-disable-next-line no-console
   if (err) return console.log(err);
   console.log(`Server running on port ${port}`);
 });
---
+++ build/415.3271e3391555ef7efee5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklanding_octopus=self.webpackChunklanding_octopus||[]).push([[415],{4415:(n,u,e)=>{e.r(u),e.d(u,{default:()=>t});var s=e(5893);const t=function(){return(0,s.jsx)("div",{children:"AboutPage"})}}}]);
