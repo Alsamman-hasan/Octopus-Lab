a2c75b9521b00ef0c9f3d26b60874dcb983f1f25
Hasan Ali Alsamman <hassan.alsamman2015@gmail.com>
2022-10-04 17:33:31 +0300

    lang switcher

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -3,22 +3,29 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTranslation } from "react-i18next"
 import { useState } from "react";
+
+import { useLocation } from "react-router-dom";
 import cls from "./LangSwitcher.module.scss";
 
 export interface LangSwitcherProps {
   className?: string;
 }
+
 export const LangSwitcher = ({ className }: LangSwitcherProps) => {
   const {i18n } = useTranslation()
   const [lang, setLang] = useState("en");
 
+
   const changeLanguage = async (lng: string) => {
     setLang(lng);
     i18n.changeLanguage(lng);
-  } 
+  }
+
+  const { pathname } = useLocation();
+  const isEn = pathname === "/en";
 
   return (
-    <div className={classNames(cls.LangSwitcher, {}, [className])}>
+    <div className={classNames(cls.LangSwitcher, { [cls.displaySwit]: isEn }, [className])}>
       <button
         type='button'
         onClick={() => changeLanguage("ru")}
